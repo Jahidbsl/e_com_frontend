0fcaea88955337e19fabe0ebb4cf75854273b8f8
jahid <jahidhasanbsl46@gmail.com>
2026-08-08 19:20:32 +0600

    update config for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   images: {
-    dangerouslyAllowLocalIP: true, 
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/**",
-      },
-      {
         protocol: "https",
-        hostname: "e-com-backend-lr5z.onrender.com",
+        hostname: "**",
+        port: "",
         pathname: "/**",
       },
     ],
