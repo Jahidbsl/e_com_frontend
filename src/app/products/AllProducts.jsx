100768714e214a85d5b1332551520f30eb460d7c
jahid <jahidhasanbsl46@gmail.com>
2026-08-10 19:56:16 +0600

    all product image update

--- src/app/products/AllProducts.jsx
+++ src/app/products/AllProducts.jsx
@@ -13,7 +13,11 @@ const AllProducts = ({ products }) => {
           <div>
             {product.image ? (
               <img
-                src={`${process.env.NEXT_PUBLIC_BASE_URL}${product.image}`}
+                src={
+                  product.image.startsWith("http")
+                    ? product.image
+                    : `${process.env.NEXT_PUBLIC_BASE_URL}${product.image}`
+                }
                 alt={product.name || "Product image"}
                 className="w-full h-[200px] object-cover rounded-md"
               />
